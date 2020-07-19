7b236cdd19a8e02f3f7e1546223cda3cc65d21a4
zveridze <zverevdmitri.87@gmail.com>
2020-07-19 19:32:33 +0300

    Add dockerfile, add docker-compose, add postgres settings, update readme.

--- recovery/settings.py
+++ recovery/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'recovery.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'NAME': 'postgres',
     }
 }
 
